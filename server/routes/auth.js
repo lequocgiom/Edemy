445f052bf97f529d141345c4c4aee4f828dd1c86
Trung Le <letrung95@gmail.com>
2021-06-28 22:51:19 +0700

    verify token and get current user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth";
+import { register, login, logout, currentUser } from "../controllers/auth";
+import { requireSignin } from "../middlewares";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
+router.get("/current-user", requireSignin, currentUser);
 
 module.exports = router;
