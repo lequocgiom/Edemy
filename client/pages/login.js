9b70e19bf23edf81dcca3b7eeea7a4897219fdaa
Trung Le <letrung95@gmail.com>
2021-06-28 20:53:34 +0700

    dispatch user response to context

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import { Context } from "../context";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+
+  //state
+  const { state, dispatch } = useContext(Context);
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -19,8 +23,11 @@ const Login = () => {
         password
       });
 
-      console.log("LOGIN RESPONSE", data);
       // toast.success("Registration successful. Please login");
+      dispatch({
+        type: "LOGIN",
+        payload: data
+      });
       setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
