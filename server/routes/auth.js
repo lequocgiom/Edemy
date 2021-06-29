b25bccae620d426133f792b2e676ece72a74f208
Trung Le <letrung95@gmail.com>
2021-06-29 14:48:43 +0700

    sending test email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { register, login, logout, currentUser } from "../controllers/auth";
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+  sendTestEmail
+} from "../controllers/auth";
 import { requireSignin } from "../middlewares";
 
 const router = express.Router();
@@ -8,5 +14,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/current-user", requireSignin, currentUser);
+router.get("/send-email", sendTestEmail);
 
 module.exports = router;
