f0419a616a70d2b877dee799436ffdc14642bdd3
Trung Le <letrung95@gmail.com>
2021-07-06 23:30:52 +0700

    conditional links and pages

---
+++ client/pages/user/become-instructor.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const BecomeInstructor = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center square">Become Instructor</h1>
+    </>
+  );
+};
+
+export default BecomeInstructor;
