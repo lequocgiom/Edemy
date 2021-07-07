ffd1cf458a159474eb29981a011f29521fe6c9de
Trung Le <letrung95@gmail.com>
2021-07-07 16:14:21 +0700

    Delete image s3 server

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { uploadImage } from "../controllers/course";
+import { uploadImage, removeImage } from "../controllers/course";
 import { requireSignin } from "../middlewares";
 
 const router = express.Router();
 
 router.post("/course/upload-image", uploadImage);
+router.post("/course/remove-image", removeImage);
 
 module.exports = router;
