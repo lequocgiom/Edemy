a9aeec4bbe5418a156dffc7ec6d3c7940a0632e7
Trung Le <letrung95@gmail.com>
2021-07-07 11:58:08 +0700

    create course component

---
+++ client/components/forms/CourseCreateForm.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+import { Select, Button } from "antd";
+const { Option } = Select;
+const CourseCreateForm = ({
+  handleSubmit,
+  values,
+  setValues,
+  handleChange,
+  handleImage
+}) => {
+  return (
+    <form obSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Name"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <textarea
+          name="description"
+          value={values.description}
+          cols="7"
+          rows="7"
+          className="form-control"
+          onChange={handleChange}
+        ></textarea>
+      </div>
+
+      <div className="form-row">
+        <div className="col">
+          <div className="form-group">
+            <Select
+              style={{ width: "100%" }}
+              size="large"
+              value={values.paid}
+              onChange={v => setValues({ ...values, paid: !values.paid })}
+            >
+              <Option value={true}>Paid</Option>
+              <Option value={false}>Free</Option>
+            </Select>
+          </div>
+        </div>
+      </div>
+
+      <div className="form-row">
+        <div className="col">
+          <div className="form-group">
+            <label className="btn btn-outline-secondary btn-block text-left">
+              {values.loading ? "Uploading" : "Image Upload"}
+              <input
+                type="file"
+                name="image"
+                onChange={handleImage}
+                accept="image/*"
+                hidden
+              />
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <Button
+            onClick={handleSubmit}
+            disabled={values.loading || values.uploading}
+            className="btn btn-primary"
+            loading={values.loading}
+            type="primary"
+            size="large"
+            shape="round"
+          >
+            {values.loading ? "Saving..." : "Save & Continue"}
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CourseCreateForm;
--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
+import CourseCreateForm from "../../../components/forms/CourseCreateForm";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
-import { Select, Button } from "antd";
-import { SaveOutlined } from "@ant-design/icons";
-
-const { Option } = Select;
 
 const CourseCreate = () => {
   const [values, setValues] = useState({
@@ -28,85 +24,19 @@ const CourseCreate = () => {
     console.log(values);
   };
 
-  const courseCreateFrom = () => (
-    <form obSubmit={handleSubmit}>
-      <div className="form-group">
-        <input
-          type="text"
-          name="name"
-          className="form-control"
-          placeholder="Name"
-          value={values.name}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="form-group">
-        <textarea
-          name="description"
-          value={values.description}
-          cols="7"
-          rows="7"
-          className="form-control"
-          onChange={handleChange}
-        ></textarea>
-      </div>
-
-      <div className="form-row">
-        <div className="col">
-          <div className="form-group">
-            <Select
-              style={{ width: "100%" }}
-              size="large"
-              value={values.paid}
-              onChange={v => setValues({ ...values, paid: !values.paid })}
-            >
-              <Option value={true}>Paid</Option>
-              <Option value={false}>Free</Option>
-            </Select>
-          </div>
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="col">
-          <div className="form-group">
-            <label className="btn btn-outline-secondary btn-block text-left">
-              {values.loading ? "Uploading" : "Image Upload"}
-              <input
-                type="file"
-                name="image"
-                onChange={handleImage}
-                accept="image/*"
-                hidden
-              />
-            </label>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <Button
-            onClick={handleSubmit}
-            disabled={values.loading || values.uploading}
-            className="btn btn-primary"
-            loading={values.loading}
-            type="primary"
-            size="large"
-            shape="round"
-          >
-            {values.loading ? "Saving..." : "Save & Continue"}
-          </Button>
-        </div>
-      </div>
-    </form>
-  );
-
   return (
     <InstructorRoute>
       <h1 className="jumbotron text-center square">Create Course</h1>
-      <div className="pt-3 pb-3">{courseCreateFrom()}</div>
+      <div className="pt-3 pb-3">
+        <CourseCreateForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          handleImage={handleChange}
+          values={values}
+          setValues={setValues}
+        />
+      </div>
+      <pre>{JSON.stringify(values, null, 4)}</pre>
     </InstructorRoute>
   );
 };
