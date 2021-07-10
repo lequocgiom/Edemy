633f4e7439211c65cc2087c89e427a10b56eff51
Trung Le <letrung95@gmail.com>
2021-07-10 15:03:49 +0700

    display single course info

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -2,16 +2,51 @@ import React from "react";
 import axiso from "axios";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { Badge } from "antd";
+import { currencyFormatter } from "../../utils/helpers";
 
 const SingleCourse = ({ course }) => {
   const router = useRouter();
   const { slug } = router.query;
 
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category
+  } = course;
+
   return (
     <>
-      <div className="container-fluid">
+      <div className="jumbotron bg-primary square">
         <div className="row">
-          <pre>{JSON.stringify(course, null, 4)}</pre>
+          <div className="col-md-8">
+            <h1 className="text-light font-weight-bold">{name}</h1>
+            <p className="lead">{description?.substring(0, 100)}...</p>
+            <Badge
+              count={category}
+              style={{ backgroundColor: "#03a9f4" }}
+              className="pb-4 mr-2"
+            />
+            <p>Created by {instructor.name}</p>
+            <p>Last updated {new Date(updatedAt).toLocaleDateString()}</p>
+            <h4 className="text-light">
+              {paid
+                ? currencyFormatter({
+                    amount: price,
+                    currency: "usd"
+                  })
+                : "Free"}
+            </h4>
+          </div>
+          <div className="col-md-4">
+            <p>show course image</p>
+          </div>
         </div>
       </div>
     </>
