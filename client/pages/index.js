90e431d61b8e05e12d9261cfaa83ec4021210188
Trung Le <letrung95@gmail.com>
2021-07-10 12:26:34 +0700

    Published courses client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Index = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const { data } = await axios.get(`/api/courses`);
+      setCourses(data);
+    };
+    fetchCourses();
+  }, []);
   return (
     <>
       <h1 className="jumbotron text-center bg-primary square">
         Online Education Marketplace
       </h1>
-      <p>from next js...</p>
+      <div className="container-fluiod">
+        <div className="row">
+          {courses.map(course => (
+            <div key={course._id} className="col-md-4">
+              <pre>{JSON.stringify(course, null, 4)}</pre>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
