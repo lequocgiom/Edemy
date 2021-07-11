1ca6a3ebcc0993cea305e63e5db9f13e0d7ef471
Trung Le <letrung95@gmail.com>
2021-07-11 15:39:19 +0700

    Single course jumbotron

---
+++ client/components/cards/SingleCourseJumbotron.js
@@ -0,0 +1,79 @@
+import { Badge, Modal } from "antd";
+import React from "react";
+import { currencyFormatter } from "../../utils/helpers";
+import ReactPlayer from "react-player";
+
+const SingleCourseJumbotron = ({
+  course,
+  setPreview,
+  preview,
+  showModal,
+  setShowModal
+}) => {
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category
+  } = course;
+  return (
+    <div className="jumbotron bg-primary square">
+      <div className="row">
+        <div className="col-md-8">
+          {/* title */}
+          <h1 className="text-light font-weight-bold">{name}</h1>
+          {/* description */}
+          <p className="lead">{description?.substring(0, 100)}...</p>
+          {/* category */}
+          <Badge
+            count={category}
+            style={{ backgroundColor: "#03a9f4" }}
+            className="pb-4 mr-2"
+          />
+          {/* instructor */}
+          <p>Created by {instructor.name}</p>
+          {/* last updated */}
+          <p>Last updated {new Date(updatedAt).toLocaleDateString()}</p>
+          {/* price */}
+          <h4 className="text-light">
+            {paid
+              ? currencyFormatter({
+                  amount: price,
+                  currency: "usd"
+                })
+              : "Free"}
+          </h4>
+        </div>
+        <div className="col-md-4">
+          {lessons[0].video && lessons[0].video.Location ? (
+            <div
+              onClick={() => {
+                setPreview(lessons[0].video.Location);
+                setShowModal(!showModal);
+              }}
+            >
+              <ReactPlayer
+                className="react-player-div"
+                url={lessons[0].video.Location}
+                light={image.Location}
+                width="100%"
+                height="225px"
+              />
+            </div>
+          ) : (
+            <>
+              <img src={image.Location} alt={name} className="img img-fluid" />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleCourseJumbotron;
--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-import axiso from "axios";
-import { useRouter } from "next/router";
 import axios from "axios";
-import { Badge, Modal } from "antd";
-import { currencyFormatter } from "../../utils/helpers";
-import ReactPlayer from "react-player";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import SingleCourseJumbotron from "../../components/cards/SingleCourseJumbotron";
 
 const SingleCourse = ({ course }) => {
   const router = useRouter();
@@ -13,70 +10,15 @@ const SingleCourse = ({ course }) => {
   const [showModal, setShowModal] = useState(false);
   const [preview, setPreview] = useState("");
 
-  const {
-    name,
-    description,
-    instructor,
-    updatedAt,
-    lessons,
-    image,
-    price,
-    paid,
-    category
-  } = course;
-
   return (
     <>
-      <div className="jumbotron bg-primary square">
-        <div className="row">
-          <div className="col-md-8">
-            <h1 className="text-light font-weight-bold">{name}</h1>
-            <p className="lead">{description?.substring(0, 100)}...</p>
-            <Badge
-              count={category}
-              style={{ backgroundColor: "#03a9f4" }}
-              className="pb-4 mr-2"
-            />
-            <p>Created by {instructor.name}</p>
-            <p>Last updated {new Date(updatedAt).toLocaleDateString()}</p>
-            <h4 className="text-light">
-              {paid
-                ? currencyFormatter({
-                    amount: price,
-                    currency: "usd"
-                  })
-                : "Free"}
-            </h4>
-          </div>
-          <div className="col-md-4">
-            {lessons[0].video && lessons[0].video.Location ? (
-              <div
-                onClick={() => {
-                  setPreview(lessons[0].video.Location);
-                  setShowModal(!showModal);
-                }}
-              >
-                <ReactPlayer
-                  className="react-player-div"
-                  url={lessons[0].video.Location}
-                  light={image.Location}
-                  width="100%"
-                  height="225px"
-                />
-              </div>
-            ) : (
-              <>
-                <img
-                  src={image.Location}
-                  alt={name}
-                  className="img img-fluid"
-                />
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-
+      <SingleCourseJumbotron
+        course={course}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        preview={preview}
+        setPreview={setPreview}
+      />
       {showModal ? course.lessons[0].video.Location : "dont show"}
     </>
   );
