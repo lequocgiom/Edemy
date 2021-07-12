0112afa715281e7e39d65d72a4de783b6d067599
Trung Le <letrung95@gmail.com>
2021-07-12 20:11:23 +0700

    stripe success page

---
+++ client/pages/stripe/success/[id].js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import UserRoute from "../../../components/routes/UserRoute";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { SyncOutlined } from "@ant-design/icons";
+
+const StripeSuccess = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (id) successRequest();
+  }, [id]);
+
+  const successRequest = async () => {
+    const { data } = await axios.get(`/api/stripe-success/${id}`);
+    router.push(`/user/course/${data.slug}`);
+  };
+  return (
+    <UserRoute showNav={false}>
+      <div className="row text-center">
+        <div className="col-md-9 pb-5">
+          <div className="d-flex justify-content-center p-5">
+            <SyncOutlined spin className="display-1 text-danger p-5" />
+          </div>
+        </div>
+        <div className="col-md-3"></div>
+      </div>
+    </UserRoute>
+  );
+};
+
+export default StripeSuccess;
